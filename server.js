d0e28b415f733c0c22311d0de242f643193c24f1
Seth Wooten <slwooten14@gmail.com>
2022-03-24 14:46:12 -0400

    added environment variable

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express'); // express is what we just installed //
 const path = require('path'); // path allows us to join paths // 
 
 const app = express(); // execute express ///
-const PORT = 3001; // set constant for port number // all caps means we should never change it // 
+const PORT = process.env.PORT || 3001; // set constant for port number // all caps means we should never change it // 
 
 app.use(express.static('public')); // middle ware - what happens inbetween our requests and our responses // serve a static directory called 'public' //
 app.use(express.json());
