a646d8f152582cb478ef8ebd0da726965f2fe5f1
Seth Wooten <slwooten14@gmail.com>
2022-03-25 21:06:44 -0400

    app.get working, added app.post but not yet functional

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'); // express is what we just installed // 
 const path = require('path'); // path allows us to join paths // 
+const fs = require('fs');
 
 const app = express(); // execute express ///
 const PORT = process.env.PORT || 3001; // set constant for port number // all caps means we should never change it // 
@@ -13,11 +14,40 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+// reads db.json, returns all saved notes as JSON //
+app.get('/api/notes', (req, res) => {
+  fs.readFile('/db/db.json', 'utf-8', (err, data) => {
+    res.json(JSON.parse(data))
+  })
+});
+
+app.post('/api/notes', (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+    };
+
+    const noteString = JSON.stringify(newNote, null, 2);
+
+    fs.appendFileSync('db/db.json', noteString);
+    
+    const response = {
+      body: newNote,
+    };
+
+    res.json(response);
+  }
+  
+});
 
 app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`) // makes the app work // listens for any requests at the following path
+  console.log(`Example app listening at http://localhost:${PORT}`)
 );
