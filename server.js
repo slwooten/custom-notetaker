4e883bd72f81f2c6ff5051c97c249da7f63725dd
Seth Wooten <slwooten14@gmail.com>
2022-05-07 13:48:38 -0400

    notes saving to json file

--- server.js
+++ server.js
@@ -1,41 +1,59 @@
 const express = require('express'); // express is what we just installed // 
 const path = require('path'); // path allows us to join paths // 
 const fs = require('fs');
+const { randomUUID } = require('crypto');
 
 const app = express(); // execute express ///
 const PORT = process.env.PORT || 3001; // set constant for port number // all caps means we should never change it // 
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(express.static('public')); // middle ware - what happens inbetween our requests and our responses // serve a static directory called 'public' //
-
-// reads db.json, returns all saved notes as JSON //
-app.get('/api/notes', (req, res) => {
-  
-  fs.readFile('./db/db.json', 'utf-8', function(err, data) {
-    console.log('data!!', JSON.parse(data))
+app.get('/api/notes', function (req, res) {
+  fs.readFile('./db/db.json', 'utf-8', function (err, data) {
     res.json(JSON.parse(data))
   })
-});
+})
 
-app.post('api/notes', (req, res) => {
+app.post('/api/notes', function (req, res) {
+  const { title, text } = req.body;
 
-  fs.readFile('./db/db.json', 'utf-8', function(err, data) {
-    res.json(JSON.parse(data))
-  })
-});
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: randomUUID()
+    };
 
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedNotes = JSON.parse(data);
 
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
+        parsedNotes.push(newNote);
 
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 2),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.log('Successfully updated notes')
+        );
+      }
+    })
+  }
+})
 
+app.get('/notes', function (req, res) {
+  res.sendFile(path.join(__dirname, './public', 'notes.html'));
+})
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './public', 'index.html'));
+})
 
 
 
